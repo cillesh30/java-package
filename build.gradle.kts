e2844f55f093f37523d92320f0cac66f3085fdb5
cillesh30 <cillesh30@gmail.com>
2024-04-17 04:24:04 +0200

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
     implementation("org.apache.commons:commons-collections4:4.4")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+// https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+implementation("org.apache.commons:commons-lang3:3.13.0")
+
 }
 
 tasks.test {
